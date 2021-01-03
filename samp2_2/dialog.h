8939c65b1617c4fab350386766831a8e1d6fcd37
seeforwhat <15158195831@163.com>
2021-01-03 10:22:34 +0800

    samp2_2/

---
+++ samp2_2/dialog.h
@@ -0,0 +1,30 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Dialog; }
+QT_END_NAMESPACE
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+
+private slots:
+    void on_chkBoxUnder_clicked(bool checked);
+
+    void on_chkBoxItalic_clicked(bool checked);
+
+    void on_chkBoxBold_clicked(bool checked);
+
+    void setTextFontColor();
+
+private:
+    Ui::Dialog *ui;
+};
+#endif // DIALOG_H
